22450ea3cbf7d62099b73417c46f8b94d067232b
srinivasarao <aleti@everest.engineering>
2024-05-06 15:50:05 +0530

    Add make scripts to run applications
    Signed-off-by: srinivasarao <aleti@everest.engineering>

--- tools/make/golang.mk
+++ tools/make/golang.mk
@@ -1,5 +1,25 @@
-.PHONY: go-test
-go-test: ## Run go unit tests
-go-test:
+# This is a wrapper to build golang binaries
+#
+# All make targets related to golang are defined in this file.
+
+# Supported Platforms for building multiarch binaries.
+PLATFORMS ?= darwin_amd64 darwin_arm64 linux_amd64 linux_arm64 
+CONFIG ?= 
+
+.PHONY: test
+test: ## Run go unit tests
+test:
 	@go test ./...
 
+
+.PHONY: build
+build:
+build: ## Run Go Build
+	@echo "building application, please wait..."
+	@go build -o bin/data-sync-manager ./orchestrator
+
+.PHONY: run
+run: build
+run: ## Run application
+	@echo "running application..."
+	@bin/data-sync-manager --config $(CONFIG)
