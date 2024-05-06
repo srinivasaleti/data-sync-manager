42fac94bfd8f4910ca2b7a2499c32f9de472d0b2
srinivasarao <aleti@everest.engineering>
2024-05-06 14:54:19 +0530

    Remove go test coverage
    Signed-off-by: srinivasarao <aleti@everest.engineering>

--- tools/make/golang.mk
+++ tools/make/golang.mk
@@ -1,7 +1,5 @@
-.PHONY: go-test-coverage
-go-test-coverage: ## Run go unit tests
-go-test-coverage:
-	@go test $(shell  go list ./... | grep -v 'mocks') -coverprofile=coverage.xml
-	@go tool cover -html coverage.xml -o cover.html
-	open cover.html
+.PHONY: go-test
+go-test: ## Run go unit tests
+go-test:
+	@go test ./...
 
